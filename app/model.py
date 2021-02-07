626012813212b3fecb9b51ce72053c0c820c00f1
Catalin <catalin.craita21@gmail.com>
2021-02-07 22:51:12 +0200

    API functions and model

--- app/model.py
+++ app/model.py
@@ -1,13 +1,16 @@
 from tensorflow import keras
 import pickle
+import numpy as np
+from nltk.tokenize import word_tokenize
+from unidecode import unidecode
+from nltk.stem.snowball import RomanianStemmer
 
-TITLE_SENT_SIZE = 10
-TEXT_SENT_SIZE = 100
+SENT_SIZE = 100
 CHAR_FEAT_SIZE = 10
 
 def title_and_text_model(title_char2idx, text_char2idx):
     # TITLE INPUT
-    title_input = keras.layers.Input(shape=(TITLE_SENT_SIZE, CHAR_FEAT_SIZE))
+    title_input = keras.layers.Input(shape=(SENT_SIZE, CHAR_FEAT_SIZE))
     title_char_emb = keras.layers.TimeDistributed(keras.layers.Embedding(input_dim=len(title_char2idx) + 1,
         output_dim=30, input_length=CHAR_FEAT_SIZE))(title_input)  
 
@@ -18,7 +21,7 @@ def title_and_text_model(title_char2idx, text_char2idx):
     title_char_feats = keras.layers.TimeDistributed(keras.layers.Flatten())(title_char_maxpool)
     
     # TEXT INPUT
-    text_input = keras.layers.Input(shape=(TEXT_SENT_SIZE, CHAR_FEAT_SIZE))
+    text_input = keras.layers.Input(shape=(SENT_SIZE, CHAR_FEAT_SIZE))
     text_char_emb = keras.layers.TimeDistributed(keras.layers.Embedding(input_dim=len(text_char2idx) + 1,
         output_dim=30, input_length=CHAR_FEAT_SIZE))(text_input)  
 
@@ -42,12 +45,52 @@ def title_and_text_model(title_char2idx, text_char2idx):
 
     return model
 
+def process(txt, char2idx):
+    def clean(txt):
+        tokens = word_tokenize(txt)
+        stemmer = RomanianStemmer()
+        # remove all tokens that are not alphabetic
+        words = [stemmer.stem(word.lower()) for word in tokens if word.isalpha()]
+        return words
+
+    def get_char_features(X, char2idx, sent_size=100, char_feat_size=10):
+
+        def word2charidxs(word, char2idx):
+            char_feats = list(map(lambda c : char2idx.get(c, 0), word))
+            return char_feats
+        
+        X_chars = []
+        for sent in X:
+            sent_indx = list(map(lambda x: word2charidxs(x, char2idx), sent))
+            sent_indx = keras.preprocessing.sequence.pad_sequences(maxlen=char_feat_size,
+                sequences=sent_indx, padding="post", truncating="post", value=0)
+            X_chars.append(sent_indx)
+        pad_val = np.zeros((sent_size, char_feat_size))
+        X_chars = keras.preprocessing.sequence.pad_sequences(maxlen=sent_size, sequences=X_chars,
+            padding="post", truncating="post", value = pad_val)
+        return X_chars
+    
+    words = clean(txt)
+
+    return get_char_features([words], char2idx)[0]
+
 def load_model(weights_path='../model.h5', char2idx_path='../char2idxs.pkl'):
     with open(char2idx_path, 'rb') as f:
         char2idxs = pickle.load(f)
     model = title_and_text_model(char2idxs['title_char2idx'], char2idxs['text_char2idx'])
+    print(model.summary())
+
     model.load_weights(weights_path)
-    return model
+    return model, char2idxs
 
 if __name__ == '__main__':
-    model = load_model()
+    model, char2idxs = load_model()
+
+    test_title = 'pula in cur'
+    processed = process(test_title, char2idxs['title_char2idx'])
+    processed = processed.reshape(1, *processed.shape)
+
+    rez = model.predict([processed, processed])
+    
+    
+    
